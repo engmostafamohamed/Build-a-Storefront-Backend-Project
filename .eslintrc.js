a681049fdecc5e967b589293d84dd12fe6a8d64d
Mostafa <Mostafa@coding.com>
2022-11-11 02:47:01 +0200

    configration eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.export = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "prettier"
+    ],
+    "rules": {
+        semi: ['error', 'never'],
+        // quotes: ['error', 'single'],
+        // 'prettier/prettier': 2, // Means error
+        // 'no-console': 1, // Means warning
+        // 'no-var': 'error',
+        // 'prefer-const': 'error',
+        'prettier/prettier': 2, // Means error
+    }
+}
