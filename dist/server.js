427c7b9e13435a8c08a1cb56e44fc9e54a37468d
Mostafa <Mostafa@coding.com>
2022-11-11 18:51:46 +0200

    add middleware

--- dist/server.js
+++ dist/server.js
@@ -2,16 +2,25 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const port = 3000;
-const app = (0, express_1.default)();
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
+var morgan_1 = __importDefault(require("morgan"));
+var helmet_1 = __importDefault(require("helmet"));
+var port = 3000;
+var app = (0, express_1["default"])();
+// http login middleware
+app.use((0, morgan_1["default"])('common'));
+//http security middleware
+app.use((0, helmet_1["default"])());
+// middleware to parse request
+app.use(express_1["default"].json());
 // start express server
-app.listen(port, () => {
-    console.log(`Server is starting at :${port}`);
+app.listen(port, function () {
+    console.log("Server is starting at :".concat(port));
 });
 // add routing for / path
 app.get("/", function (req, res) {
+    console.log(req.body);
     res.send("this is the ____ route");
 });
-exports.default = app;
+exports["default"] = app;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express, { Request, Response } from "express"
+import morgan from 'morgan';
+import helmet from 'helmet';
 const port = 3000
 const app: express.Application = express()
+// http login middleware
+app.use(morgan('common'));
+//http security middleware
+app.use(helmet());
+// middleware to parse request
+app.use(express.json());
 // start express server
 app.listen(port, () => {
     console.log(`Server is starting at :${port}`)
 })
 // add routing for / path
 app.get("/", function (req: Request, res: Response) {
+    console.log(req.body);
     res.send("this is the ____ route")
 })
 export default app
